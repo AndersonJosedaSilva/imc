c6c01fd6b141dfa41061026e4c10ef0a6e51b754
Anderson.pan <Aluno0312@LABF323448.fuvs.br>
2015-05-12 21:59:40 -0300

    added todo

--- IMCService.js
+++ IMCService.js
@@ -1,7 +1,12 @@
 var IMCService = {
-    
-    calculate: function(height,weight){
-    
-    };
-    
-    description: function()
+	
+	calculate: function(height, weight) {
+		return weight / (height * height);
+	},
+	
+	//TODO to implemented
+	description: function(index) {
+		return "To be implemented";
+	}
+	
+};
